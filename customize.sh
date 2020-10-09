b63d787685c2b28c4fb7eab4ef6b05da897ac731
xiaojing110 <62259239+xiaojing110@users.noreply.github.com>
2020-10-09 14:30:13 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,6 +5,16 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
+#1. Add Some Package
+mkdir -p package/custom && cd package/custom
+src-git helloworld https://github.com/fw876/helloworld
+src-git jerryk https://github.com/jerrykuku/openwrt-package
+src-git lienol https://github.com/Lienol/openwrt-package
+src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
 #1. Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' openwrt/package/base-files/files/bin/config_generate
 sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
